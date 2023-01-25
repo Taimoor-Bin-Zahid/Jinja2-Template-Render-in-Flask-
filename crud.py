e4efb4997ebf3e1beaf8a0dd7c77965b0008a274
TaimoorZahid86 <taimoorbinzahid86@gmail.com>
2023-01-25 17:04:46 +0500

    committed

---
+++ crud.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+# class HelloWorld(Resource):
+#     def __init__(self):
+#         pass
+
+#     def get(self):
+#         return {
+#             "Hello":"World"
+#         }
+# api.add_resource(HelloWorld,"/")
+
+
+Data = []
+
+class People(Resource):
+    def get(self, name):
+        for x in Data:
+            if x["Data"] == name:
+                return x
+        return {"Data":None}
+
+
+
+    def post(self, name):
+        Temp = {"Data":name}
+        Data.append(Temp)
+        return Temp
+
+
+
+    def delete(self,name):
+        for ind,x in enumerate(Data):
+            if x["Data"] == name:
+                Temp = Data.pop(ind)
+                return {"Note":"Deleted"}
+
+api.add_resource(People,"/Name/<string:name>")
+
+if __name__ == "__main__":
+    app.run(debug=True)
